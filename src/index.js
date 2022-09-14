0c2d5daf3087fdc4da4fcfc92e31676e8561b753
adam0457 <adam0457@algonquinlive.com>
2022-09-13 22:26:44 -0400

    Reformat the indexjs file

--- src/index.js
+++ src/index.js
@@ -1,70 +1,70 @@
+'use strict';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, doc, getDocs } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDKvKz18ehgFqqQFsGYCF0Si_FMZgE6ZIM",
-  authDomain: "fire-giftr-cad6e.firebaseapp.com",
-  projectId: "fire-giftr-cad6e",
-  storageBucket: "fire-giftr-cad6e.appspot.com",
-  messagingSenderId: "416768678849",
-  appId: "1:416768678849:web:77f6c29fec81803b46ed40"
-};
+const GIFTR = {
+   /** My global variables */  
+  firebaseConfig:{
+    apiKey: "AIzaSyDKvKz18ehgFqqQFsGYCF0Si_FMZgE6ZIM",
+    authDomain: "fire-giftr-cad6e.firebaseapp.com",
+    projectId: "fire-giftr-cad6e",
+    storageBucket: "fire-giftr-cad6e.appspot.com",
+    messagingSenderId: "416768678849",
+    appId: "1:416768678849:web:77f6c29fec81803b46ed40"
+  },
+  personList:document.querySelector('ul.person-list'),
+  people:[],
+  db:null,
+  init: () => {
+    GIFTR.addListeners();
+    let app = initializeApp(GIFTR.firebaseConfig);
+    GIFTR.db = getFirestore(app); 
+    GIFTR.getPeople();
+  },
+  // app : initializeApp(GIFTR.firebaseConfig),
+  // db : getFirestore(GIFTR.app), 
+  
+  addListeners:() => {
+        document.getElementById('btnCancelPerson').addEventListener('click', GIFTR.hideOverlay);
+      document.getElementById('btnCancelIdea').addEventListener('click', GIFTR.hideOverlay);
+      document.querySelector('.overlay').addEventListener('click', GIFTR.hideOverlay);
 
-const people = []; //to hold all the people from the collection
+      document.getElementById('btnAddPerson').addEventListener('click', GIFTR.showOverlay);
+      document.getElementById('btnAddIdea').addEventListener('click', GIFTR.showOverlay);
+      GIFTR.personList.addEventListener('click', GIFTR.showListGifts);
+  },  
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// get a reference to the database
-const db = getFirestore(app);
+  hideOverlay:(ev)=>{
+      ev.preventDefault();
+    document.querySelector('.overlay').classList.remove('active');
+    document
+      .querySelectorAll('.overlay dialog')
+      .forEach((dialog) => dialog.classList.remove('active'));
+  },
 
-document.addEventListener('DOMContentLoaded', () => {
-  //set up the dom events
-  document
-    .getElementById('btnCancelPerson')
-    .addEventListener('click', hideOverlay);
-  document
-    .getElementById('btnCancelIdea')
-    .addEventListener('click', hideOverlay);
-  document.querySelector('.overlay').addEventListener('click', hideOverlay);
+  showOverlay:(ev)=>{
+      ev.preventDefault();
+    document.querySelector('.overlay').classList.add('active');
+    const id = ev.target.id === 'btnAddPerson' ? 'dlgPerson' : 'dlgIdea';
+    //TODO: check that person is selected before adding an idea
+    document.getElementById(id).classList.add('active');
+  },
 
-  document
-    .getElementById('btnAddPerson')
-    .addEventListener('click', showOverlay);
-  document.getElementById('btnAddIdea').addEventListener('click', showOverlay);
-  getPeople();
-});
-
-function hideOverlay(ev) {
-  ev.preventDefault();
-  document.querySelector('.overlay').classList.remove('active');
-  document
-    .querySelectorAll('.overlay dialog')
-    .forEach((dialog) => dialog.classList.remove('active'));
-}
-function showOverlay(ev) {
-  ev.preventDefault();
-  document.querySelector('.overlay').classList.add('active');
-  const id = ev.target.id === 'btnAddPerson' ? 'dlgPerson' : 'dlgIdea';
-  //TODO: check that person is selected before adding an idea
-  document.getElementById(id).classList.add('active');
-}
-
-async function getPeople(){  
-  const querySnapshot = await getDocs(collection(db, 'people'));
-  querySnapshot.forEach((doc) => {
-    const data = doc.data();
-    const id = doc.id;
-    people.push({id, ...data});
+  getPeople:async()=>{
+      const querySnapshot = await getDocs(collection(GIFTR.db, 'people'));
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const id = doc.id;
+      GIFTR.people.push({id, ...data});
   });
-  buildPeople(people);
-}
+  GIFTR.buildPeople(GIFTR.people);
+  },
 
-function buildPeople(people){
-  //build the HTML
-  let ul = document.querySelector('ul.person-list');
-  // let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', ...];
+  buildPeople:(people)=>{  
+  // let ul = document.querySelector('ul.person-list');  
   let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  ul.innerHTML = people.map(person=>{
+  // ul.innerHTML = people.map(person=>{
+  GIFTR.personList.innerHTML = people.map(person=>{
     const dob = `${months[person['birth-month']-1]} ${person['birth-day']}`;
     //Use the number of the birth-month less 1 as the index for the months array
     return `<li data-id="${person.id}" class="person">
@@ -72,5 +72,10 @@ function buildPeople(people){
             <p class="dob">${dob}</p>
           </li>`;
   }).join('');
-}
+  },
 
+  showListGifts: (ev) => {
+    console.log(ev.target.closest('li').getAttribute('data-id'));
+  }
+};
+GIFTR.init();
