2ae1d9dfc6e83a93e1b934137a52faea838bc325
adam0457 <adam0457@algonquinlive.com>
2022-09-14 11:11:16 -0400

    Save a new person

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 
 'use strict';
 import { initializeApp } from 'firebase/app';
-import { query, where,getFirestore, collection, doc, getDocs } from 'firebase/firestore';
+import { query, where,getFirestore, collection, doc, getDocs, addDoc } from 'firebase/firestore';
 
 const GIFTR = {
    /** My global variables */  
@@ -20,6 +20,9 @@ const GIFTR = {
   },
   personList:document.querySelector('ul.person-list'),
   ideaList: document.querySelector('ul.idea-list'),
+  name: null,
+  month: null,
+  day: null,
   people:[],
   db:null,
   init: () => {
@@ -30,29 +33,86 @@ const GIFTR = {
   },  
   
   addListeners:() => {
-        document.getElementById('btnCancelPerson').addEventListener('click', GIFTR.hideOverlay);
+      document.getElementById('btnCancelPerson').addEventListener('click', GIFTR.hideOverlay);
       document.getElementById('btnCancelIdea').addEventListener('click', GIFTR.hideOverlay);
-      document.querySelector('.overlay').addEventListener('click', GIFTR.hideOverlay);
+      // document.querySelector('.overlay').addEventListener('click', GIFTR.hideOverlay);
 
       document.getElementById('btnAddPerson').addEventListener('click', GIFTR.showOverlay);
       document.getElementById('btnAddIdea').addEventListener('click', GIFTR.showOverlay);
+      document.getElementById('btnSavePerson').addEventListener('click', GIFTR.savePerson);
       GIFTR.personList.addEventListener('click', GIFTR.showListGifts);
-  },  
+  }, 
+  
+  savePerson: async(ev) => {
+    GIFTR.name = document.getElementById('name').value;
+    GIFTR.month = document.getElementById('month').value;
+    GIFTR.day = document.getElementById('day').value;
+    if(!GIFTR.name || !GIFTR.month || !GIFTR.day) return;
+    const person = {
+      'name': GIFTR.name,
+      'birth-month': GIFTR.month,
+      'birth-day': GIFTR.day
+    };
+
+    try {
+      const docRef = await addDoc(collection(GIFTR.db, 'people'), person );
+      console.log('Document written with ID: ', docRef.id);
+      //1. clear the form fields 
+      document.getElementById('name').value = '';
+      document.getElementById('month').value = '';
+      document.getElementById('day').value = '';
+      //2. hide the dialog and the overlay
+      GIFTR.hideOverlay();
+      //3. display a message to the user about success 
+      // tellUser(`Person ${GIFTR.name} added to database`);
+      console.log(`Person ${GIFTR.name} added to database`);
+      person.id = docRef.id;
+      //4. ADD the new HTML to the <ul> using the new object
+      GIFTR.showPerson(person);
+    } catch (err) {
+      console.error('Error adding document: ', err);
+      //do you want to stay on the dialog?
+      //display a mesage to the user about the problem
+    }
+
+  },
+
+  showPerson: (person) => {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let li = document.getElementById(person.id);
+    if(li){
+      //update on screen
+      const dob = `${months[person['birth-month']-1]} ${person['birth-day']}`;
+      //Use the number of the birth-month less 1 as the index for the months array
+      //replace the existing li with this new HTML
+      li.outerHTML = `<li data-id="${person.id}" class="person">
+              <p class="name">${person.name}</p>
+              <p class="dob">${dob}</p>
+            </li>`;
+    }else{
+      //add to screen
+      const dob = `${months[person['birth-month']-1]} ${person['birth-day']}`;
+      //Use the number of the birth-month less 1 as the index for the months array
+      li = `<li data-id="${person.id}" class="person">
+              <p class="name">${person.name}</p>
+              <p class="dob">${dob}</p>
+            </li>`;
+      document.querySelector('ul.person-list').innerHTML += li;
+  }
+  },
 
   hideOverlay:(ev)=>{
-      ev.preventDefault();
+    // ev.preventDefault();
     document.querySelector('.overlay').classList.remove('active');
-    document
-      .querySelectorAll('.overlay dialog')
-      .forEach((dialog) => dialog.classList.remove('active'));
+    document.querySelectorAll('.overlay dialog').forEach((dialog) => dialog.classList.remove('active'));
   },
 
   showOverlay:(ev)=>{
       ev.preventDefault();
-    document.querySelector('.overlay').classList.add('active');
-    const id = ev.target.id === 'btnAddPerson' ? 'dlgPerson' : 'dlgIdea';
-    //TODO: check that person is selected before adding an idea
-    document.getElementById(id).classList.add('active');
+      document.querySelector('.overlay').classList.add('active');
+      const id = ev.target.id === 'btnAddPerson' ? 'dlgPerson' : 'dlgIdea';
+      //TODO: check that person is selected before adding an idea
+      document.getElementById(id).classList.add('active');
   },
 
   getPeople:async()=>{
